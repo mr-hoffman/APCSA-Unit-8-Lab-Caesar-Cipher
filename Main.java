b64a4a9e7ad3ef673801bd29a216c7e3c0059dd7
mr-hoffman <hoffmant@hilbertk12.org>
2025-08-13 12:35:22 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+class main {
+  public static void main(String[] args) {
+
+  // write code here to test your Cipher class.
+  // when you're finished, replace your test code with
+  // the tests I provided
+
+  }
+}
